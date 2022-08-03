1b5470f1d0046ecff5fa549b2d8154ba8cde6a48
Daniel Neto <daniel.jsilvan@gmail.com>
2022-08-03 19:23:26 -0300

    Fix configure dashboards

--- 06-configure_dashboards.sh
+++ 06-configure_dashboards.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-OPENSEARCHDASH_CONFIG=$PWD/configs/opensearch_dashboards.yml
+OPENSEARCH_CONFIG_DIR=$PWD/configs
+PASSWORD_FILE=${OPENSEARCH_CONFIG_DIR}/auth_setup.out
+OPENSEARCHDASH_CONFIG=${OPENSEARCH_CONFIG_DIR}/opensearch_dashboards.yml
 
 # Configure opensearch dashboards to use new kibanaserver password
 echo "[Configure Opensearch-Dashboards]"
 
-DASHBOARDS_PASS=$(grep "kibanaserver " /etc/perfsonar/opensearch/auth_setup.out | head -n 1 | sed 's/^kibanaserver //')
+DASHBOARDS_PASS=$(grep "kibanaserver " ${PASSWORD_FILE} | head -n 1 | sed 's/^kibanaserver //')
 
 sed -i "s/opensearch.password: kibanaserver/opensearch.password: ${DASHBOARDS_PASS}/g" $OPENSEARCHDASH_CONFIG
 # Clear and then set reverse proxy settings
