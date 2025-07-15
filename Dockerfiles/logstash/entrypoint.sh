115906d15bb175aecfa468ae70088483513542dc
Daniel Neto <daniel.jsilvan@gmail.com>
2025-07-15 20:15:06 -0300

    Update Logstash pipeline to use 0.0.0.0 instead of localhost

--- Dockerfiles/logstash/entrypoint.sh
+++ Dockerfiles/logstash/entrypoint.sh
@@ -5,6 +5,9 @@ set -e
 python3 /usr/lib/perfsonar/logstash/scripts/update_logstash_pipeline_yml.py
 python3 /usr/lib/perfsonar/logstash/scripts/enable_prometheus_pipeline.py
 
+## update pipeline to use 0.0.0.0 instead of localhost
+sed -i /usr/lib/perfsonar/logstash/pipeline/01-inputs.conf -e 's/localhost/0.0.0.0/g'
+
 echo "Waiting for sysconfig file..."
 while [ ! -f /usr/lib/perfsonar/logstash/sysconfig ]; do
   sleep 2
